1e84349c21addd0ad7819a0c45f19b06e282ca36
Borja Lorente <blorente@ucm.es>
2017-07-13 00:50:00 +0200

    Add initial range table

---
+++ generateRangesTable.py
@@ -0,0 +1,60 @@
+values = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+suits = ['s', 'o']
+
+htmlTableTemplate = '<table "class"="table" cellspacing="2" cellpadding="0">\n'
+htmlCellTemplate = "\t\t\t<td \"id\"=\"(id)\" onmouseover=\"return tableClick(this);\">(content)</td>\n"
+
+def main():
+    matrix = generateMatrices()
+    #printRawMatrix(matrix)
+    formatted = formatMatrix(htmlCellTemplate, matrix)
+    print(formatted)
+
+def generateMatrices():
+    table = [[]]
+    valuesNum = len(values)
+    for y in range(0, valuesNum):
+        for x in range(0, valuesNum):
+            toAdd = ''
+            if x == y:
+                # Pocket pair
+                toAdd = values[x] + values[x]
+            elif x > y:
+                # Suited
+                toAdd = values[y] + values[x]+'s'
+            elif x < y:
+                # Offsuit
+                toAdd = values[x] + values[y]+'o'
+
+            #print('Adding %s at pos (%d, %d)' % (toAdd, x, y))
+
+            table[y].append(toAdd)
+        table.append([])
+
+    table.pop()
+    return table
+
+def printRawMatrix(matrix):
+    for line in matrix:
+        print(line)
+
+def formatMatrix(template, matrix):
+    output = htmlTableTemplate
+    x = 0
+    y = 0
+    for line in matrix:
+        output += '\t<tr>\n'
+        for cell in line:
+            id = str(x)+'-'+str(y)
+            toAdd = template
+            toAdd = toAdd.replace('(id)', id)
+            toAdd = toAdd.replace('(content)', cell)
+            output += toAdd
+            x = x + 1
+        output += '\t</tr>\n'
+        y = y + 1
+        x = 0
+    output += '</table>\n'
+    return output
+
+main()
